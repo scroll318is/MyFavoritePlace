ef64c37285e3ebfc2877dffd22d51e564d95f229
Stoyan Kostov <stoyanykostov@gmail.com>
2015-10-28 19:57:57 +0200

    added General and About screen, Photos screen in progress

--- MyFavoritePlace/SecondViewController.swift
+++ MyFavoritePlace/SecondViewController.swift
@@ -10,16 +10,11 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var blrView: UIVisualEffectView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        blrView.layer.cornerRadius = 25.0
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
